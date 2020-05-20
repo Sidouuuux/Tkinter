6da78300f039b14623ca157ea5c84bef6ccdcdd6
Sidouuuux <mirsidahmed@hotmail.fr>
2020-05-20 12:55:30 +0200

    Ajout joueurs

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import string
 from tkinter import *
+f= open("score.txt","r")
+f.close()
 
 def addPlayer():
+    entryPseudo.configure(background='white')
     pseudo = entryPseudo.get()
     print("username entered :", entryPseudo.get())
     if len(pseudo) < 4 or len(pseudo) == 0:
         entryPseudo.configure(background='red')
     else:
         print("good!")
+
+    fill = open("score.txt", "a")
+    fill.write(pseudo + "   0\n")
+    fill.close()
+    printScore()
+
+
+def printScore():
+    fill = open("score.txt", "r")
+    line = " "
+    while line:
+        line = fill.readline()
+        Label(frameRight, text=line, borderwidth=1 ).pack()
+    f.close()
 
 width = 1000
 height = 500
@@ -27,11 +44,11 @@ label = Label(frameLeft, image=photo)
 label.pack(side=RIGHT)
 frameLeft.grid(row=0, column=0, sticky=W)
 
-
 frameRight = Frame(window, width=100, height=100)
 
 labelScore = Label(frameRight, text="Score", font=("courrier", 30), fg="black")
 labelScore.pack()
+printScore()
 
 entryPseudo = Entry(frameRight, text="Score", font=("courrier", 20), fg="black")
 entryPseudo.pack(side=LEFT, padx=10)
